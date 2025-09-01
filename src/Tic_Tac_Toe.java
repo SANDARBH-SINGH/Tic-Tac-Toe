cd453bdd2c7308214c17c8baa78dc095c91c4fcd
SANDARBH SINGH <sandarbhsingh17714@gmail.com>
2025-09-01 21:53:50 +0530

    Completed Half Part

--- src/Tic_Tac_Toe.java
+++ src/Tic_Tac_Toe.java
@@ -1,5 +1,33 @@
+import java.util.*;
 public class Tic_Tac_Toe {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("***** Welcome to the game of Tic Tac Toe *****");
+        Scanner sc = new Scanner(System.in);
+        int[][] board = new int[3][3];
+        char player = 'X';
+        for(int i = 0 ; i < board.length ; i++){
+            for(int j = 0 ; j < board[i].length ; j++){
+                board[i][j] = ' ';
+            }
+        }
+        boolean gameOver = false;
+        while(!gameOver){
+            System.out.println(board);
+            System.out.println("Make your move Player :"+player);
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            if(board[row][col] == ' '){
+                board[row][col] = player;
+            }
+            gameOver = haveWon(board,player);
+            if(gameOver){
+
+            }
+        }
     }
+
+    private static boolean haveWon(int[][] board, char player) {
+
+    }
+}
 }
